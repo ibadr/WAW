de69821434f4cda86848117d64bc876076d768a7
Islam <ibadreldin@ufl.edu>
2016-10-10 21:11:43 -0400

    Add device enumeration

--- waw.h
+++ waw.h
@@ -1,19 +1,32 @@
 #ifndef __WAW_H__
 #define __WAW_H__
 
+#include <digilent/waveforms/dwf.h> // already got extern "C"
+#include <vector>
+
 class WAW
 {
 public:
-  static WAW & instance()
-  {
+  static WAW & instance() {
     static WAW instance;
     return instance;
   }
+  ~WAW() {}
+
+  int enumDevices(ENUMFILTER enumFilter) {
+    int cDevice;
+    BOOL succ;
+    succ = FDwfEnum(enumfilterDiscovery, &cDevice);
+    return succ ? cDevice : -1;
+  }
 
 private:
-  WAW();
-  WAW(WAW const &);
-  void operator=(WAW const &);
+  WAW() {}
+  WAW(WAW const &) {}
+  void operator=(WAW const &) {}
+
+  // std::vector<HDWF> _hdwfVec;
+  // std::vector<int> _clientsVec;
 };
 
 #endif
